d0d16c22f3c8ee308a63747ed37b33c7f7535ff1
ThisisSal-star <njorogewambui32@gmail.com>
2025-01-26 15:36:06 +0300

    Added an id on html

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+function searchCity (city){
+    let apiKey = "ad031o47ac65t5df81db4b9e365cb480";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+    axios.get(apiUrl). then (refreshWeather);
+
+}
+
+
 function submitForm (event){
 event.preventDefault();
 let searchinput=document.querySelector("#search-input");
 let cityelement = document.querySelector("#weather-city");
 cityelement.innerHTML = searchinput.value;
+searchCity(searchinput.valu);
 }
 
 let searchForm = document.querySelector ("#Search-form");
