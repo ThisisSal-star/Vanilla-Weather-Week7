80cfdae87d43bdd67b797e88b42b7322462485cc
ThisisSal-star <njorogewambui32@gmail.com>
2025-01-27 12:47:07 +0300

    update on html

--- src/index.js
+++ src/index.js
@@ -1,25 +1,56 @@
+function updateWeather (response){
+
+
+let tempElement = document.querySelector("#weather-temp");
+let temperature = response.data.temperature.current;
+let cityElement = document.querySelector("#weather-city");
+let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windSpeedElement = document.querySelector("#wind-speed");
+let timeElement + document.querySelector("#time");
+let date = new date (response.data.time * 1000);
+
+cityElement.innerHTML = response.data.city;
+timeElement.innerHTML = `${date.getDay()} ${date.getHours()}: ${date.getMinutes}`;
+descriptionElement.innerHTML = response.data.condition.description;
+humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+tempElement.innerHTML = Math.round(temperature);
+
+}
+
+function formatDate(date){
+    let days = date.getday();
+    let hours = date.getHours();
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    let days =days [date.gateDay()];
+    return "Monday 12:49";
+}
 function searchCity (city){
     let apiKey = "ad031o47ac65t5df81db4b9e365cb480";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
     axios.get(apiUrl). then (updateWeather);
 
 }
 
-let tempElement = document.querySelector("#weather-temp");
-let temperature = Response.data.temperature.current;
-let cityElement = document.querySelector("#weather-city");
-
-cityElement.innerHTML = Response.data.city;
-tempElement.innerHTML = Math.round(temperature);
 
 
 function submitForm (event){
 event.preventDefault();
 let searchinput=document.querySelector("#search-input");
 
-searchCity(searchinput.valu);
+searchCity(searchinput.value);
 }
 
 let searchForm = document.querySelector ("#Search-form");
 searchForm.addEventListener("submit", submitForm);
+
