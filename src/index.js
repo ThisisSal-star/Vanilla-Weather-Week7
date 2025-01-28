35e99eb5eff433c3723f8d2caade8ad77d2b95e5
ThisisSal-star <njorogewambui32@gmail.com>
2025-01-28 16:35:57 +0300

    Inserted icon url, updated js and css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
+function searchCity (city){
+    let apiKey = "ad031o47ac65t5df81db4b9e365cb480";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+    axios.get(apiUrl). then (updateWeather);
+
+}
+
+
+
 function updateWeather (response){
 
 
-let tempElement = document.querySelector("#weather-temp");
+let temperatureElement = document.querySelector("#weather-temp");
 let temperature = response.data.temperature.current;
 let cityElement = document.querySelector("#weather-city");
 let descriptionElement = document.querySelector("#description");
@@ -9,13 +19,17 @@ let humidityElement = document.querySelector("#humidity");
 let windSpeedElement = document.querySelector("#wind-speed");
 let timeElement = document.querySelector("#time");
 let date = new Date (response.data.time * 1000);
+let iconElement = document.querySelector("#icon")
+
 
 cityElement.innerHTML = response.data.city;
 timeElement.innerHTML = formatDate(date);
 descriptionElement.innerHTML = response.data.condition.description;
 humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-tempElement.innerHTML = Math.round(temperature);
+temperatureElement.innerHTML = Math.round(temperature);
+iconElement.innerHTML =`<img src ="${response.data.condition.icon_url} "class="weather-icon" `
+
 
 }
 
@@ -34,18 +48,14 @@ function formatDate(date){
     ];
     let day =days [date.gateDay()];
 
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
    
     return `${day} ${hours}:${minutes}`;
 
 }
     
-function searchCity (city){
-    let apiKey = "ad031o47ac65t5df81db4b9e365cb480";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-
-    axios.get(apiUrl). then (updateWeather);
-
-}
 
 
 
@@ -58,3 +68,5 @@ searchCity(searchinput.value);
 
 let searchForm = document.querySelector ("#Search-form");
 searchForm.addEventListener("submit", submitForm);
+
+searchCity("Paris");
